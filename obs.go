e017c8f5823d16b33d45254ff4607304566a4174
Peter Teichman <peter@teichman.org>
2015-04-19 19:46:06 -0700

    Rename old2, old3 -> had2, had3 in obs2.Observe

--- obs.go
+++ obs.go
@@ -8,13 +8,13 @@ func (o obs1) Observe(tok0 token, tok1 token) {
 
 type obs2 map[bigram]tokset
 
-func (o obs2) Observe(ctx bigram, tok2 token) (old2, old3 bool) {
-	set, old2 := o[ctx]
+func (o obs2) Observe(ctx bigram, tok2 token) (had2, had3 bool) {
+	set, had2 := o[ctx]
 
-	if newset, old3 := set.Add(tok2); !old3 {
+	if newset, had3 := set.Add(tok2); !had3 {
 		o[ctx] = newset
-		return old2, false
+		return had2, false
 	}
 
-	return old2, true
+	return had2, true
 }
